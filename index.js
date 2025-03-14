4c8aea86d245a281e5049c769fb791009eef610b
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-14 21:51:23 +1100

    fetching single movia via id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'ee9fd5c4',
-      s: 'avengers'
+      i: 'tt0848228'
     }
   });
 
