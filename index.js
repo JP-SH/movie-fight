7869c9dea4c7969d961f0ea22f0544d1c66bbffc
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-17 16:55:53 +1100

    moving HTML for dropdown into JS file

--- index.js
+++ index.js
@@ -13,8 +13,20 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const input = document.querySelector('input');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label> <b> Search for a Movie </b> </label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
 
+const input = document.querySelector('input');
+const dropwdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 
 const onInput =  async event => {
