9dcbbc7033b97ff3514e20335093e7bce26f5685
Jasdeep Singh <j.singhhh917@gmail.com>
2025-05-26 17:24:47 +1000

    simple mocha test in the browser

---
+++ test/autocomplete.test.js
@@ -0,0 +1,6 @@
+// if you want to write a test with mocha you call the 'it' functiion
+// mocha makes this function globally available so you dont have to require it in
+
+it('works!!', () => {
+
+});
