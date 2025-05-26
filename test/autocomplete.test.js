4564fbefdead8e1d35c27fa78f1737fa3828da7a
Jasdeep Singh <j.singhhh917@gmail.com>
2025-05-26 17:42:12 +1000

    didplaying autocomplete for testing

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,6 +1,18 @@
 // if you want to write a test with mocha you call the 'it' functiion
 // mocha makes this function globally available so you dont have to require it in
 
-it('works!!', () => {
-
+it('Shows an autocomplete', () => {
+  createAutoComplete({
+    root: document.querySelector('#target'),
+    fetchData() {
+      return [
+        { Title: 'Avengers' },
+        { Title: 'The Nice Guys' },
+        { Title: 'Dune part ||' }
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  });
 });
